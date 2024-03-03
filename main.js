5724490a4150e1c15cfcd05652b83f1d64a05397
THEJUSKRISHNAN <thejus12a2003@gmail.com>
2024-03-04 01:03:55 +0530

    commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick =()=>{
-    menuIcon.classList.toggle('fa.xmark');
+    menuIcon.classList.toggle('fa-x');
     navbar.classList.toggle('active');
 }
+{/* <i class="fa-solid "></i> */}

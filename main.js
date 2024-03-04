1b3cf88f4d77b9a590d1bafc3589bb5c9f4e0591
THEJUSKRISHNAN <thejus12a2003@gmail.com>
2024-03-04 20:34:48 +0530

    commit

--- main.js
+++ main.js
@@ -6,40 +6,10 @@ menuIcon.onclick =()=>{
     navbar.classList.toggle('active');
 }
 
-// // scrolll selecton active link
-// let selectons = document.querySelectorAll('section');
-// let navLink = document.querySelectorAll('header nav a');
-// window.onscroll=()=>{
-//     selectons.forEach(sec =>{
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
 
-//         if(top >-offset && top < offset+height)
-//         {
-//             navLink.forEach.apply(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('header nav a[href*=' +id + ']').classList.add('active');
-
-//             });
-//         };
-//     })
-
-    // sticky nav bar
-
-//     let header=document.querySelector('header');
-//     header.classList.toggle('sticky',window.scrollY>100);
-
-//     // remove togle icon and navbar
-//     menuIcon.classList.remove('fa-x');
-//     navbar.classList.remove('active');
-//  }
-
-//  scrol reveal
 
 ScrollReveal({
-    distance: '80px',
+    distance: '50px',
     duration:2000,
     delay:200,
 
